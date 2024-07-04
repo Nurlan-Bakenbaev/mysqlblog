fd35c054311e7d5d1bbb3cba97f1eb686d6895cc
Nurlan <iamsamurai1991@gmail.com>
2024-07-04 12:33:46 +0200

    modified:   src/app/write/page.jsx 	new file:   src/helpers/react-quill.js

--- src/app/write/page.jsx
+++ src/app/write/page.jsx
@@ -2,43 +2,17 @@
 import React, { useState } from "react";
 import ReactQuill from "react-quill";
 import "react-quill/dist/quill.snow.css";
+import { modules, formats } from "@/helpers/react-quill";
 const WritePost = () => {
   const [postValue, setPostValue] = useState("");
-
-  const modules = {
-    toolbar: [
-      [{ header: "1" }, { header: "2" }, { font: [] }],
-      [{ list: "ordered" }, { list: "bullet" }],
-      ["bold", "italic", "underline"],
-      ["image"],
-      [{ align: [] }],
-      [{ color: [] }, { background: [] }],
-    ],
-  };
-
-  const formats = [
-    "header",
-    "font",
-    "size",
-    "bold",
-    "italic",
-    "underline",
-    "list",
-    "bullet",
-    "image",
-    "color",
-    "background",
-    "align",
-  ];
   console.log(postValue);
   return (
     <div>
-      <form>
+      <form className="px-6 mt-8 flex flex-col gap-5">
         <input
           className="border w-full py-4 pl-3"
           type="text"
-          placeholder="Post title"
-        />
+          placeholder="Post title"/>
         <ReactQuill
           modules={modules}
           formats={formats}
---
+++ src/helpers/react-quill.js
@@ -0,0 +1,25 @@
+export const modules = {
+  toolbar: [
+    [{ header: "1" }, { header: "2" }, { font: [] }],
+    [{ list: "ordered" }, { list: "bullet" }],
+    ["bold", "italic", "underline"],
+    ["image"],
+    [{ align: [] }],
+    [{ color: [] }, { background: [] }],
+  ],
+};
+
+export const formats = [
+  "header",
+  "font",
+  "size",
+  "bold",
+  "italic",
+  "underline",
+  "list",
+  "bullet",
+  "image",
+  "color",
+  "background",
+  "align",
+];
